117dc687f7cb8c8d17ef8e9dd51c4967cbb69213
masud3386 <masud3386@yahoo.com>
2024-01-06 21:47:24 -0500

    commit

---
+++ src/test/java/question/Testfacebook.java
@@ -0,0 +1,30 @@
+package question;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Testfacebook {
+	WebDriver driver;
+	@Test
+	public void login() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("masud3386@yahoo.com");
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("madudd");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.quit();
+		
+		
+	}
+
+}
